2f1f9de3e749a3ab51aa6d982652964ad7625588
julio@yola.com <julio@yola.com>
2012-02-13 15:33:04 -0800

    Make requests compatible with urllib2
    Added a monkey patch to replace the urllib2.build_opener which can cause KeyErrors.

--- requests/__init__.py
+++ requests/__init__.py
@@ -32,3 +32,55 @@ from .exceptions import (
     RequestException, Timeout, URLRequired,
     TooManyRedirects, HTTPError, ConnectionError
 )
+
+# The following patch allows requests to be compatible with urllib2 
+def _patched_build_opener(*handlers):
+    """Create an opener object from a list of handlers.
+
+    The opener will use several default handlers, including support
+    for HTTP and FTP.
+
+    If any of the handlers passed as arguments are subclasses of the
+    default handlers, the default handlers will not be used.
+    """
+    import types
+    import httplib
+    from urllib2 import OpenerDirector, ProxyHandler, UnknownHandler, HTTPHandler, HTTPDefaultErrorHandler, HTTPRedirectHandler, FTPHandler, FileHandler, HTTPErrorProcessor
+    def isclass(obj):
+        return isinstance(obj, types.ClassType) or hasattr(obj, "__bases__")
+
+    try:
+        from urllib2 import HTTPSHandler
+    except ImportError:
+        pass
+
+    opener = OpenerDirector()
+    default_classes = [ProxyHandler, UnknownHandler, HTTPHandler,
+                       HTTPDefaultErrorHandler, HTTPRedirectHandler,
+                       FTPHandler, FileHandler, HTTPErrorProcessor]
+    if hasattr(httplib, 'HTTPS'):
+        default_classes.append(HTTPSHandler)
+    skip = []
+    for klass in default_classes:
+        for check in handlers:
+            if isclass(check):
+                if issubclass(check, klass):
+                    skip.append(klass)
+            elif isinstance(check, klass):
+                skip.append(klass)
+    for klass in skip:
+        if klass in default_classes:  # This simple if statement is the source of all my troubles...
+            default_classes.remove(klass)
+
+    for klass in default_classes:
+        opener.add_handler(klass())
+
+    for h in handlers:
+        if isclass(h):
+            h = h()
+        opener.add_handler(h)
+    return opener
+
+# Monkey patch that bad library
+import urllib2
+urllib2.build_opener = _patched_build_opener
